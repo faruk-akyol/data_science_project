b2cced8687e66f754026e4079519cf3da4ea7b8c
faruk-akyol <farukakyol93@gmail.com>
2023-10-02 15:43:15 +0300

    glassdoor data

---
+++ data_cleaning.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 15:36:30 2023
+
+@author: faruk
+"""
+import os
+import pandas as pd 
+import numpy as np
+
+
+pd.read_csv("glassdoor_jobs.csv")
+
+os.getcwd()
